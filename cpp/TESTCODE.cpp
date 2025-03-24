6e88847f0b9259d99d768244113779bb75772d75
Mirek Pesta <mirekpesta@gmail.com>
2025-03-24 14:39:50 +0100

    ...

---
+++ cpp/TESTCODE.cpp
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include <string> 
+
+// open //home/pi/Documents/AdruinoAtariPaddle/cpp  and run program manualy
+
+int main( void )
+{
+	
+int a=9;
+int b=3;
+
+
+
+int d = a % b; 
+
+printf("Printing Integer value %d", d); 
+
+
+
+//* Likely uses the result of the division.
+}
